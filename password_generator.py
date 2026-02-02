c37407029e7c95b2421fada97147a10d95c4a280
Aleksei Zaitsev <ztsv.aleksei@gmail.com>
2026-02-02 12:59:19 +0000

    added conditions to password generator

--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,22 @@
 import random
 import string
+
 def generate_password(length):
-    """A random password is generated"""
+    if length < 1:
+        raise ValueError("length must be at least 1")
+    if length > 100:
+        raise ValueError("length must be no more than 100")
+
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choices(characters, k=length))
     return password
 
-
 if __name__ == "__main__":
-    length = int(input("Enter the number of characters: "))
-    password = generate_password(length)
-    print("A password has been generated:")
-    print(password)
+    length = int(input("enter the number of characters: "))
+
+    try:
+        password = generate_password(length)
+        print("a password has been generated:")
+        print(password)
+    except ValueError as e:
+        print(e)
